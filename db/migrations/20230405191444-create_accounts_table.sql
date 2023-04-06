f4ea745cb2c2f3f66c03ba457f0d282cccd0c86e
lbragadev <louigibraga@gmail.com>
2023-04-05 20:29:39 -0700

    implemented accounts migration

--- db/migrations/20230405191444-create_accounts_table.sql
+++ db/migrations/20230405191444-create_accounts_table.sql
@@ -1,4 +1,14 @@
 
 -- +migrate Up
+CREATE TABLE IF NOT EXISTS accounts
+(
+    id        serial primary key NOT NULL,
+    first_name            varchar(50) NOT NULL,
+    last_name            varchar(50) NOT NULL,
+    number serial,
+    balance serial,
+    created_at timestamp
+);
 
 -- +migrate Down
+DROP TABLE IF EXISTS accounts;
